901708c3a74e547b3ad89459cdfd93f480315dde
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2024-03-02 19:00:41 +0500

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -8,9 +8,9 @@
 
     $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
 
-    $status = mailfunction("", "Company", $body); //reciever
+    $status = mailfunction("azizov.mirkhakimjon@gmail.com", "Company", $body); //reciever
     if($status)
-        echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
+        echo '<center><h1>Rahmat! Tez orada siz bilan bog\'lanamiz.</h1></center>';
     else
-        echo '<center><h1>Error sending message! Please try again.</h1></center>';    
+        echo '<center><h1>Xabar yuborishda xatolik yuz berdi! Iltimos, yana bir bor urinib ko\'ring.</h1></center>';    
 ?>
