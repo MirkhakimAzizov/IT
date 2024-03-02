57480564b6f33ad0b4eea5a5b610428b5c64589f
Mirkhakim Azizov <121539952+MirkhakimAzizov@users.noreply.github.com>
2024-03-02 18:57:09 +0500

    Update careers.php

--- careers.php
+++ careers.php
@@ -19,13 +19,13 @@
 	{
 		$status = mailfunction("azizov.mirkhakimjon@gmail.com", "Company", $body, $filenameWithDirectory); //reciever
         if($status)
-            echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
+            echo '<center><h1>Xabar yuborganigiz uchun rahmat, tez orada siz bilan bog\'lanamiz.</h1></center>';
         else
-            echo '<center><h1>Error sending message! Please try again.</h1></center>';
+            echo '<center><h1>Xabar yuborishda xatolik yuz berdi! Iltimos, yana bir bor urinib ko\'ring.</h1></center>';
 	}
 	else 
 	{
-		echo "<center><h1>Error uploading file! Please try again.</h1></center>";
+		echo "<center><h1>Faylni yuklashda xatolik yuz berdi! Iltimos, yana bir bor urinib ko'ring.</h1></center>";
 	}
 
 ?>
